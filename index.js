f3772d0daf10e3848d1588e2170cb32d1eb239c0
viny <viny0698@gmail.com>
2023-01-10 17:48:23 +0530

    install standardJS for formatting

--- index.js
+++ index.js
@@ -1,78 +1,76 @@
 const seats = {
-    seats: [
-        {
-            seatNumber: 1,
-            bookedBy: null
-        },
-        {
-            seatNumber: 2,
-            bookedBy: null
-        },
-        {
-            seatNumber: 3,
-            bookedBy: null
-        },
-        {
-            seatNumber: 4,
-            bookedBy: null
-        }
-    ],
-    totalSeats: 4,
-    unbookedSeats: 4
+  seats: [
+    {
+      seatNumber: 1,
+      bookedBy: null
+    },
+    {
+      seatNumber: 2,
+      bookedBy: null
+    },
+    {
+      seatNumber: 3,
+      bookedBy: null
+    },
+    {
+      seatNumber: 4,
+      bookedBy: null
+    }
+  ],
+  totalSeats: 4,
+  unbookedSeats: 4
 }
 
 const inputParser = (input) => {
-    const [totalPassengersString, ...passengersStrings] = input.split('\n')
+  const [totalPassengersString, ...passengersStrings] = input.split('\n')
 
-    parsedPassengers = passengersStrings.map(passenger => {
-        const [name, ageString, gender] = passenger.split(' ')
-        return {name, age: parseInt(ageString, 10), gender}
-    })
-    return {totalPassengers: parseInt(totalPassengersString, 10), passengers: parsedPassengers}
+  parsedPassengers = passengersStrings.map(passenger => {
+    const [name, ageString, gender] = passenger.split(' ')
+    return { name, age: parseInt(ageString, 10), gender }
+  })
+  return { totalPassengers: parseInt(totalPassengersString, 10), passengers: parsedPassengers }
 }
 
 const main = (seats, input) => {
+  const { totalPassengers, passengers } = inputParser(input)
+  const { bookings, newSeats } = bookSeats(JSON.stringify(seats), totalPassengers, passengers)
 
-    const {totalPassengers, passengers} = inputParser(input)
-    const {bookings, newSeats} = bookSeats(JSON.stringify(seats), totalPassengers, passengers)
-
-    const output = bookings.length ? `Seats alloted: ` + bookings.join(' ') : 'Failed, seats are not available'
-    return output
+  const output = bookings.length ? 'Seats alloted: ' + bookings.join(' ') : 'Failed, seats are not available'
+  return output
 }
 
 const bookSeats = (seats, totalPassengers, passengers) => {
-    const newSeats = JSON.parse(seats)
-    const bookedSeats = seats.bookedSeats
+  const newSeats = JSON.parse(seats)
+  const bookedSeats = seats.bookedSeats
 
-    if (totalPassengers > bookedSeats) {
-        return {
-            bookings: [],
-            newSeats
-        }
-    }
-
-    const bookings = []
-    let counter = 0
-
-
-    newSeats.seats.forEach((seat) => {
-        if (!seat.bookedBy && counter < passengers.length) {
-            bookings.push('S' + seat.seatNumber)
-            seat.bookedBy = passengers[counter].name
-            counter++
-        }
-    })
-
-    newSeats.unbookedSeats = newSeats.unbookedSeats - passengers.length
-
+  if (totalPassengers > bookedSeats) {
     return {
-        bookings,
-        newSeats
+      bookings: [],
+      newSeats
     }
+  }
+
+  const bookings = []
+  let counter = 0
+
+  newSeats.seats.forEach((seat) => {
+    if (!seat.bookedBy && counter < passengers.length) {
+      bookings.push('S' + seat.seatNumber)
+      seat.bookedBy = passengers[counter].name
+      counter++
+    }
+  })
+
+  newSeats.unbookedSeats = newSeats.unbookedSeats - passengers.length
+
+  return {
+    bookings,
+    newSeats
+  }
 }
 
 module.exports = {
-    main,
-    inputParser,
-    bookSeats
+  main,
+  inputParser,
+  bookSeats
 }
--- index.test.js
+++ index.test.js
@@ -1,60 +1,58 @@
 const { main, inputParser, bookSeats } = require('./index')
 
 const mockAvailableSeats = {
-    seats: [
-        {
-            seatNumber: 1,
-            bookedBy: null
-        }
-    ],
-    totalSeats: 1,
-    unbookedSeats: 1
+  seats: [
+    {
+      seatNumber: 1,
+      bookedBy: null
+    }
+  ],
+  totalSeats: 1,
+  unbookedSeats: 1
 }
 
 const mockUnavailableSeats = {
-    seats: [
-        {
-            seatNumber: 1,
-            bookedBy: 'Person1'
-        }
-    ],
-    totalSeats: 1,
-    unbookedSeats: 0
+  seats: [
+    {
+      seatNumber: 1,
+      bookedBy: 'Person1'
+    }
+  ],
+  totalSeats: 1,
+  unbookedSeats: 0
 }
 
-test('return new seats data and bookings', () => {    
-    expect(bookSeats(JSON.stringify(mockAvailableSeats),
-    1, 
-    [{name: 'Viny', age: 24, gender: 'F'}])
-    )
+test('return new seats data and bookings', () => {
+  expect(bookSeats(JSON.stringify(mockAvailableSeats),
+    1,
+    [{ name: 'Viny', age: 24, gender: 'F' }])
+  )
     .toStrictEqual({
-        bookings: ['S1'],
-        newSeats: {
-            seats: [
-                {
-                    seatNumber: 1,
-                    bookedBy: 'Viny'
-                }
-            ],
-            totalSeats: 1,
-            unbookedSeats: 0
-        }
+      bookings: ['S1'],
+      newSeats: {
+        seats: [
+          {
+            seatNumber: 1,
+            bookedBy: 'Viny'
+          }
+        ],
+        totalSeats: 1,
+        unbookedSeats: 0
+      }
     })
 })
 
-
 test('book seat (available)', () => {
-    expect(main(mockAvailableSeats, '1\nViny Selopal 24 F'))
+  expect(main(mockAvailableSeats, '1\nViny Selopal 24 F'))
     .toBe('Seats alloted: S1')
 })
 
 test('parse input', () => {
-    expect(inputParser('1\nViny 24 F'))
-    .toStrictEqual({totalPassengers: 1, passengers: [{name: 'Viny', age: 24, gender: 'F'}]})
+  expect(inputParser('1\nViny 24 F'))
+    .toStrictEqual({ totalPassengers: 1, passengers: [{ name: 'Viny', age: 24, gender: 'F' }] })
 })
 
 test('book seat (unavailable)', () => {
-    expect(main(mockUnavailableSeats, '1\nViny Selopal 24 F'))
+  expect(main(mockUnavailableSeats, '1\nViny Selopal 24 F'))
     .toBe('Failed, seats are not available')
 })
-
